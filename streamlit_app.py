a5ef6afb035510d79bb849a7e879a8143d62eb0e
Ajay Iyer <92404808+ajiy01@users.noreply.github.com>
2023-08-15 16:07:27 -0700

    Update streamlit_app.py
    Added few more menu options

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
-import streamlit
+import streamlit as st
 
-streamlit.title('My Parents New Healthy Diner')
+st.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+st.header('Breakfast Favorites')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+st.text('🥣 Omega 3 & Blueberry Oatmeal')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+st.text('🥗 Kale, Spinach & Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg')
+st.text('🐔 Hard-Boiled Free-Range Egg')
+
+st.text('🥑🍞 Avocado Toast')
+
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
