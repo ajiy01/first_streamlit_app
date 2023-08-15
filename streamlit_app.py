c179537c6e110daf5e4c74c0e1f8ef5b2a476e04
Ajay Iyer <92404808+ajiy01@users.noreply.github.com>
2023-08-15 16:16:02 -0700

    Update streamlit_app.py
    imported pandas
    added a list of fruits for smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 
 st.title('My Parents New Healthy Diner')
@@ -13,3 +14,6 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
 
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
